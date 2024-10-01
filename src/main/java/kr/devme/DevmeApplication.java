e5fa8e887e55ff3d92f91d5640a1f100a38c8f7a
ksngh <chzhqk98@naver.com>
2024-10-01 19:17:52 +0900

    initial commit

---
+++ src/main/java/kr/devme/DevmeApplication.java
@@ -0,0 +1,13 @@
+package kr.devme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/devme/DevmeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.devme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
